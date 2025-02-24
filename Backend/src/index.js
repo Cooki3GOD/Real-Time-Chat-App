693957e91100f618dd4cd7cb554d9e9cffcd7951
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-02-24 20:03:30 +0100

    Add registration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
+dotenv.config();
 const app = express();
-
-app.listen(5001, () => {
-    console.log("Server is running at port 5001");
-    
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+    console.log("Server is running at port " + PORT);
+    connectDB();
 })
